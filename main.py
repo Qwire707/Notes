b99eb5245aff9b87488dc7b8450ebd028c611a3a
Qwire707 <danolovitryak299@gmail.com>
2024-01-27 20:26:02 +0200

    file

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+
 from PyQt5.QtCore import Qt
 from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QLabel, QListWidget, QLineEdit,\
     QTextEdit, QInputDialog, QHBoxLayout, QVBoxLayout
